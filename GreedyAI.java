4330f7b6f0e969f90c1083b7d876b37e7d1f22f0
halelavigezer <avigezerhalel@gmail.com>
2024-11-28 19:51:25 +0200

    Changes by shirb

--- GreedyAI.java
+++ GreedyAI.java
@@ -1,10 +1,28 @@
+import java.util.List;
+
 public class GreedyAI extends AIPlayer {
+
     public GreedyAI(boolean isPlayerOne) {
         super(isPlayerOne);
     }
 
     @Override
     public Move makeMove(PlayableLogic gameStatus) {
-        return null;
+        if (gameStatus.ValidMoves() == null) {// איו מהלכים חוקיים
+            return null;
+        }
+        List<Position> validPositions = gameStatus.ValidMoves(); // קבלת כל הפוזיציות האפשריות
+        GameLogic gameLogic = new GameLogic()
+        Disc[][] board = gameStatus.();
+        Move bestMove = null;
+        int maxFlipped = 0;
+        for (Position position : validPositions) {
+            // חשב את מספר הדיסקיות שיהפכו במהלך הזה
+            int flippedDiscs = gameLogic.countFlips(position);
+
+        }return null;
+    }
+    }
     }
 }
+
